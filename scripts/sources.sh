d6de0c9333666004c504c75f774cd7e5e4ea5ab8
tiaga <10153548+tiaga@users.noreply.github.com>
2025-10-02 07:00:21 +0000

    build: use absolute path

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -4,25 +4,22 @@ set -e
 
 COMMIT_SING_BOX="01b72e129794acae89e1c7929d0ba5a63b0e67f8"
 COMMIT_LIBNEKO="1c47a3af71990a7b2192e03292b4d246c308ef0b"
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
 
-if [[ -d ../../sing-box ]]; then
-  if [[ "$(git -C ../../sing-box rev-parse HEAD)" != "$COMMIT_SING_BOX" ]]; then
+if [[ -d "$SCRIPT_DIR/../sing-box" ]]; then
+  if [[ "$(git -C "$SCRIPT_DIR/../sing-box" rev-parse HEAD)" != "$COMMIT_SING_BOX" ]]; then
     echo "ERROR: Wrong 'sing-box' commit hash!"
   fi
 else
-  mkdir -v ../../sing-box && cd ../../sing-box
-  git clone --no-checkout https://github.com/MatsuriDayo/sing-box "$(pwd)"
-  git -c advice.detachedHead=false checkout "$COMMIT_SING_BOX"
-  cd - &>/dev/null
+  git clone --no-checkout https://github.com/MatsuriDayo/sing-box "$SCRIPT_DIR/../sing-box"
+  git --git-dir="$SCRIPT_DIR/../sing-box/.git" -C "$SCRIPT_DIR/../sing-box" -c advice.detachedHead=false checkout "$COMMIT_SING_BOX"
 fi
 
-if [[ -d ../../libneko ]]; then
-  if [[ "$(git -C ../../libneko rev-parse HEAD)" != "$COMMIT_LIBNEKO" ]]; then
+if [[ -d "$SCRIPT_DIR/../libneko" ]]; then
+  if [[ "$(git -C "$SCRIPT_DIR/../libneko" rev-parse HEAD)" != "$COMMIT_LIBNEKO" ]]; then
     echo "ERROR: Wrong 'libneko' commit hash!"
   fi
 else
-  mkdir -v ../../libneko && cd ../../libneko
-  git clone --no-checkout https://github.com/MatsuriDayo/libneko "$(pwd)"
-  git -c advice.detachedHead=false checkout "$COMMIT_LIBNEKO"
-  cd - &>/dev/null
+  git clone --no-checkout https://github.com/MatsuriDayo/libneko "$SCRIPT_DIR/../libneko"
+  git --git-dir="$SCRIPT_DIR/../libneko/.git" -C "$SCRIPT_DIR/../libneko" -c advice.detachedHead=false checkout "$COMMIT_LIBNEKO"
 fi
