7f34b30d5577a84a9700707fb7b167b8924a8b79
tiaga <10153548+tiaga@users.noreply.github.com>
2025-10-10 21:20:18 +0000

    build: do not include vcs info

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,6 +26,7 @@ gomobile init
 gomobile bind -v \
               -androidapi 21 \
               -trimpath \
+              -buildvcs=false \
               -ldflags='-s -w -buildid=' \
               -tags='with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech' .
 
