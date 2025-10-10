e30ecf95175dba8f0d7a23c2edbb052867981207
tiaga <10153548+tiaga@users.noreply.github.com>
2025-10-10 23:58:27 +0300

    build: make build reproducible (#6)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,28 +6,27 @@ set -e
 
 source $SCRIPT_DIR/env.sh
 
-bash $SCRIPT_DIR/sources.sh
-
 if [ -z "$GOPATH" ]; then
-    GOPATH=$(go env GOPATH)
+  GOPATH=$(go env GOPATH)
 fi
 
 PATH="$PATH:$GOPATH/bin"
-GOMOBILE_VERSION="v0.0.0-20231127183840-76ac6878050a"
+GOMOBILE_URL=github.com/sagernet/gomobile
+GOMOBILE_VERSION="v0.1.4"
 
 cd "$SCRIPT_DIR/../"
 
 # Install gomobile
 if [ ! -f "$GOPATH/bin/gomobile" ]; then
-  go get -v "golang.org/x/mobile@$GOMOBILE_VERSION"
-  go install -v "$GOPATH/pkg/mod/golang.org/x/mobile@$GOMOBILE_VERSION/cmd/gomobile"
+  go get -v "$GOMOBILE_URL@$GOMOBILE_VERSION"
+  go install -v "$GOPATH/pkg/mod/$GOMOBILE_URL@$GOMOBILE_VERSION/cmd/gomobile"
 fi
 
 gomobile init
 gomobile bind -v \
               -androidapi 21 \
               -trimpath \
-              -ldflags='-s -w' \
+              -ldflags='-s -w -buildid=' \
               -tags='with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech' .
 
 rm -v "$GOPATH/bin/gomobile" "$GOPATH/bin/gobind" "$SCRIPT_DIR/../libcore-sources.jar"
